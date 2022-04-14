ca1c2308c60297698601110f9fe6003b964673d8
dbirdflyshi <dander92@me.com>
2022-04-14 16:06:22 -0400

    add crafting

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,20 @@
 class Craft {
-
+    constructor(objs){
+        this.name = objs.type;
+        this.materials = objs.materials;
+        this.completed = false;
+    }
+    completeCraft(){
+        this.completed = true;
+        return 'All done! It looks great!'
+    }
+    calculateProjectTotal(){
+        var total = 0;
+        for(let i=0; i < this.materials.length; i++){
+            total += this.materials[i].calculateMaterialCost()
+        }
+        return total;
+    }
 }
 
 module.exports = Craft;
