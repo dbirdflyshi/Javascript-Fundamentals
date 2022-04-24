d3eca2f8ae0ea66ff6dc8a17b3059a22ffc29084
dbirdflyshi <dander92@me.com>
2022-04-24 17:05:26 -0400

    add vending machine

--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,5 +1,40 @@
-class VendingMachine {
+var Snack = require("../src/snack.js");
 
+class VendingMachine {
+    constructor(objs){
+        this.id = objs.id;
+        this.isBroken = objs.isBroken;
+        this.snacks = [];
+    }
+    addSnacks(snack){
+        if(this.snacks.length > 0){
+            for (let i = 0; i < this.snacks.length; i++) {
+                if (this.snacks[i].name != snack.name) {
+                    this.snacks.push(snack);
+                } else {
+                    return 'Sorry, that snack is already stocked! Try adding a different snack.'
+                }
+            }
+        } else {
+            this.snacks.push(snack);
+        }
+    }
+    purchaseSnack(name,money){
+        for (let i = 0; i < this.snacks.length; i++) {
+            if (this.snacks[i].name === name) {
+                if(this.snacks[i].itemsInStock > 0){
+                    if(this.snacks[i].price < money){
+                        this.snacks[i].removeItem();
+                        return `Success! Here is $${money - this.snacks[i].price} back!`
+                    }else{
+                        return `Sorry, not enough payment. Please add more money.`
+                    }
+                }else{
+                    return `Sorry, no items in stock. Try another item.`
+                }
+            }
+        }
+    }
 }
 
 module.exports = VendingMachine;
